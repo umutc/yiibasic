4b7729182f84072f6bedb9fc314667b0b8969359
Umut ÇELİK <mail@umutcelik.com.tr>
2016-02-10 23:56:21 +0200

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=ad_2a3f185b4b9dbb1',
+    'username' => 'b4b05733fbc194',
+    'password' => '441253cc',
+    'charset' => 'utf8',
+];
